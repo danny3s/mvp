7b293298a2eb6ca75c71b6eb87118d142050497b
Daniel <dahusa.92@gmail.com>
2018-10-31 17:46:42 -0500

    send

--- src/pages/item-details-parallax/item-details-parallax.ts
+++ src/pages/item-details-parallax/item-details-parallax.ts
@@ -28,69 +28,7 @@ export class ItemDetailsPageParallax {
                   "title": "ORR Available",
                   "button": "1500"
               },
-              {
-                  "id": 2,
-                  "avatar": "assets/images/avatar/17.jpg",
-                  "title": "Logan Rogers",
-                  "subtitle": "@logan",
-                  "button": "Follow"
-              },
-              {
-                  "id": 3,
-                  "avatar": "assets/images/avatar/18.jpg",
-                  "title": "Gary Russell",
-                  "subtitle": "@gary",
-                  "button": "Follow"
-              },
-              {
-                  "id": 4,
-                  "avatar": "assets/images/avatar/19.jpg",
-                  "title": "Amy Lee",
-                  "subtitle": "@amy",
-                  "button": "Follow"
-              },
-              {
-                  "id": 5,
-                  "avatar": "assets/images/avatar/20.jpg",
-                  "title": "Silvia Salvador",
-                  "subtitle": "@silvia",
-                  "button": "Follow"
-              },
-              {
-                  "id": 6,
-                  "avatar": "assets/images/avatar/15.jpg",
-                  "title": "Alexandra Parker",
-                  "subtitle": "@alexandra",
-                  "button": "Follow"
-              },
-              {
-                  "id": 7,
-                  "avatar": "assets/images/avatar/22.jpg",
-                  "title": "Gabriel Mascareñas",
-                  "subtitle": "@gabriel",
-                  "button": "Follow"
-              },
-              {
-                  "id": 8,
-                  "avatar": "assets/images/avatar/14.jpg",
-                  "title": "Sofia Clarke",
-                  "subtitle": "@stephen",
-                  "button": "Follow"
-              },
-              {
-                  "id": 9,
-                  "avatar": "assets/images/avatar/21.jpg",
-                  "title": "Lisa Green",
-                  "subtitle": "@lisa",
-                  "button": "Follow"
-              },
-              {
-                  "id": 10,
-                  "avatar": "assets/images/avatar/23.jpg",
-                  "title": "Danijel Bilic",
-                  "subtitle": "@danijel",
-                  "button": "Follow"
-              }
+            
           ]
 
     };
